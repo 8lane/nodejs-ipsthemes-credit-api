db46e998731d6ba36a800553c84e570a84aaa241
Tom  Christian <tctc91@gmail.com>
2017-02-02 22:41:42 +0000

    wordpress edd endpoint added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
+const request    = require('request');
 const IPSCreditScraper = require('ips-credit-scraper');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,28 +10,47 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 8080;
 const router = express.Router();
 
-router.use(function(req, res, next) {
-  console.log('Processing...');
-  next();
-});
+const config = {
+  ipsthemes: {
+    url: 'http://dev/ipsthemes2/wordpress/edd-api/v1/stats/'
+    //url: 'https://ipsthemes.com/wordpress/edd-api/v1/stats/'
+  }
+}
 
-router.route('/credit/:userName/:userPass')
-  .get(function(req, res) {
-    let scrape = new IPSCreditScraper({
-      auth: req.params.userName,
-      password: req.params.userPass
-    })
+/* IPS Themes Wordpress EDD API Sales */
+router.route('/ipsthemes/credit/:key/:token')
+  .get((req, res) => fetchIPSThemesCredit(req, res));
 
-    scrape
-      .then((credit) => res.json(credit))
-      .catch((err) => res.json(err));
-  });
+/* IPS Marketplace scraped account credit */
+router.route('/ipsmarketplace/credit/:userName/:userPass')
+  .get((req, res) => fetchIPSCredit(req, res));
 
-router.get('/', function(req, res) {
-  res.json({ message: 'IPS Themes API :-)' });
-});
+/* Default API response */
+router.get('/', (req, res) => res.json({ message: 'IPS Themes API :-)' }));
 
+/* API Prefix */
 app.use('/api', router);
+
+function fetchIPSThemesCredit(req, res) {
+  let requestOptions = {
+    url: config.ipsthemes.url,
+    qs: {
+      key: req.params.key,
+      token: req.params.token,
+      type: 'earnings'
+    }
+  }
+
+  request(requestOptions, (error, response, body) => res.send(body));
+}
+
+function fetchIPSCredit(req, res) {
+  let scrape = new IPSCreditScraper({ auth: req.params.userName, password: req.params.userPass })
+
+  return scrape
+    .then((credit) => res.json(credit))
+    .catch((err) => res.json(err));
+}
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
